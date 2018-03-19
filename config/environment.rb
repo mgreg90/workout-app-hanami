9c9ab80712cf6a8f97f24f9a89d59addfb8603dc
Mike Gregory <mgregory8219@gmail.com>
2018-03-18 23:23:24 -0400

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/workout_app_hanami'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/workout_app_hanami_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/workout_app_hanami_development'
+    #    adapter :sql, 'mysql://localhost/workout_app_hanami_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/workout_app_hanami/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
---
+++ lib/workout_app_hanami.rb
@@ -0,0 +1,2 @@
+module WorkoutAppHanami
+end
